dd24d9799652acd3c3f1eeeb72bff45c7db57c87
Patrick Wolleb <patrick.wolleb@shmedia.co.uk>
2014-12-04 15:43:44 +0000

    Cleans exmaple
    Adds grunt wercker box

--- example/paging-link_example.js
+++ example/paging-link_example.js
@@ -7,11 +7,6 @@
 
 var PagingLink = require('../lib/paging-link.js');
 
-var paging = new PagingLink({
-	root : 'http://localhost:8000/users'
-});
-
-
 var total = 34,
 		limit = 10,
 		offset = 10;
@@ -26,10 +21,17 @@ console.info('------------------------------------------------------------------
 
 
 
+// Instanciate
+var paging = new PagingLink({
+	root : 'http://localhost:8000/users',
+	limitKey : 'limit',
+	offsetKey : 'offset'
+});
+
+
 
 // Process plain data object sync 
 var header = paging.process( total, limit, offset );
-console.info('=> Sync')
 console.log(header)
 
 
